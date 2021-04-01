21e4219ec460620f1d9ce485a92120bf6a264395
BejanEduard <bejan.eduard16@yahoo.com>
2021-04-01 12:25:53 +0300

    Authentication functionality

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -1,15 +1,18 @@
 
-
-class Conflict(Exception):
-
+class HTTPException(Exception):
     def __init__(self, *args, **kwargs):
         self.status = kwargs["status"]
         del kwargs["status"]
         super().__init__(*args, **kwargs)
 
 
-class InvalidBody(Exception):
-    def __init__(self, *args, **kwargs):
-        self.status = kwargs["status"]
-        del kwargs["status"]
-        super().__init__(*args, **kwargs)
+class Conflict(HTTPException):
+    pass
+
+
+class InvalidBody(HTTPException):
+    pass
+
+
+class Unauthorized(HTTPException):
+    pass
